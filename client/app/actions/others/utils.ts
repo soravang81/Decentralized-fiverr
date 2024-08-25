fbfdd67e1218173f5243fad0365975e1f8be0a1e
soravang81 <soravang81@gmail.com>
2024-08-25 20:09:54 +0530

    deploy to vercel

--- client/app/actions/others/utils.ts
+++ client/app/actions/others/utils.ts
@@ -3,7 +3,7 @@
 import fs from "fs"
 
 export const getOwnerSecretKey = (): number[] => {
-    const WALLET_PATH = process.env.WALLET_PATH || '/home/sourav/.config/solana/id.json';
+    const WALLET_PATH = process.env.NEXT_PUBLIC_OWNER_WALLET || '/home/sourav/.config/solana/id.json';
     const secretKeyString = fs.readFileSync(WALLET_PATH, 'utf8');
     return JSON.parse(secretKeyString);
 };
