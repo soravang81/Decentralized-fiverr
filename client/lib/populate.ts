5c2c9a8e4df3ab954d06448768bfe85a40e17919
soravang81 <soravang81@gmail.com>
2024-09-01 18:19:31 +0530

    added disputes

--- client/lib/populate.ts
+++ client/lib/populate.ts
@@ -3,8 +3,6 @@ import { Category, Niche, SubNiche } from "../lib/niches"
 import { CreateGigInput, CreateOrderInput, CreatePricingPackageInput, CreateSellerProfileInput, PackageType } from "@/lib/types";
 import { createGig } from "@/app/actions/seller/gigs";
 // import { createPricingPackage } from "@/app/actions/seller/pricingPackage";
-import { createOrder } from "@/app/actions/buyer/orders";
-import { PublicKey } from "@solana/web3.js";
 
 export const dummysellerProfile:CreateSellerProfileInput = {
   userId : "8b7e45c9-b65d-4676-b3fd-570b2756c8c7",
@@ -19,10 +17,10 @@ export const dummysellerProfile:CreateSellerProfileInput = {
 export const dummypricing:CreatePricingPackageInput = {
   // gigId : "02e824fa-c464-4a9d-be05-beaf002cf46f",
   // packageType : PackageType.BASIC,
-  price : 100,
+  price : (100).toString(),
   name : "myStandard",
   description : "dssdfsdfsd",
-  deliveryTime : 3,
+  deliveryTime : (3).toString(),
   features : ["bakchodi"]
 }
 export const dummygig:CreateGigInput = {
